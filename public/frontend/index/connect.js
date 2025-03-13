35b8d67e11d2071283d67771780867c45e1deb69
Darkeuhh <darckdemforge@gmail.com>
2025-03-13 16:57:32 +0100

    ça remarche

---
+++ public/frontend/index/connect.js
@@ -0,0 +1,22 @@
+import { connectWithCookie } from "../global.js";
+
+const logButton = document.getElementById("log-button");
+connectWithCookie();
+
+const displayConnectButton = () => {
+  const is_connect = sessionStorage.getItem("IsConnect");
+
+  if (is_connect == "true") {
+    logButton.classList.add("connected");
+    logButton.classList.remove("log");
+    logButton.textContent = "profil";
+  } else {
+    logButton.classList.remove("connected");
+    logButton.classList.add("log");
+    logButton.textContent = "connexion";
+  }
+};
+
+// Partie code
+
+displayConnectButton();
--- public/frontend/index/index.js
+++ public/frontend/index/index.js
@@ -12,26 +12,3 @@ document.addEventListener("DOMContentLoaded", () => {
     window.location.href = adress + "/game/start";
   });
 });
-
-import { connectWithCookie } from "../global.js";
-
-const logButton = document.getElementById("log-button");
-connectWithCookie();
-
-const displayConnectButton = () => {
-  const is_connect = sessionStorage.getItem("IsConnect");
-
-  if (is_connect == "true") {
-    logButton.classList.add("connected");
-    logButton.classList.remove("log");
-    logButton.textContent = "profil";
-  } else {
-    logButton.classList.remove("connected");
-    logButton.classList.add("log");
-    logButton.textContent = "connexion";
-  }
-};
-
-// Partie code
-
-displayConnectButton();
