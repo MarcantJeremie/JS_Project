474bb83bfee3ed1224e566753748730a5196aae4
Clément MASQUELIER <clement.masquelier@student.junia.com>
2025-03-17 08:23:27 +0100

    fuzbf

--- public/frontend/profile/profile.js
+++ public/frontend/profile/profile.js
@@ -6,6 +6,11 @@ const selected_button = document.getElementById("selected-button");
 const form_connection = document.getElementById("form-connection");
 const form_register = document.getElementById("form-register");
 
+adress = window.location.href;
+adress = adress.split("/");
+adress = adress[adress.length - 2];
+adress = "http://" + adress;
+
 const changeSelectionLoginPage = () => {
   selected_button.classList.toggle("left");
   selected_button.classList.toggle("right");
@@ -23,9 +28,18 @@ selection_button.addEventListener("click", changeSelectionLoginPage);
 const back_to_menu_button = document.getElementById("back-to-menu");
 
 back_to_menu_button.addEventListener("click", () => {
-  adress = window.location.href;
-  adress = adress.split("/");
-  adress = adress[adress.length - 2];
-  adress = "http://" + adress;
+  
   window.location.href = adress + "/";
+});
+
+form_register.addEventListener("submit", (e) => {
+  form_data = new FormData(form_register);
+  login = form_data.get("R-login");
+  password = form_data.get("R-password");
+  email = form_data.get("email");
+
+  fetch(adress+"/register", {
+
+
+  })
 });
