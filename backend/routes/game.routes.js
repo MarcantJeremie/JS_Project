8c721336ec8d0253f5808b87b247bf0c672dc86b
MarcantJeremie <144245347+MarcantJeremie@users.noreply.github.com>
2025-03-21 09:42:05 +0100

    création de la page lobby

--- backend/routes/game.routes.js
+++ backend/routes/game.routes.js
@@ -1,15 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const publicPath = require('path').join(__dirname, '../../public');
+const publicPath = require("path").join(__dirname, "../../public");
 
-router.get('/start', (req, res) => {
-  res.sendFile(publicPath+'/frontend/begin/begin.html');
+router.get("/start", (req, res) => {
+  res.sendFile(publicPath + "/frontend/begin/begin.html");
 });
 
-router.get('/game', (req, res) => {
-  res.sendFile(publicPath+'/frontend/game/game.html');
+router.get("/game", (req, res) => {
+  res.sendFile(publicPath + "/frontend/game/game.html");
 });
 
+router.get("/lobby", (req, res) => {
+  res.sendFile(publicPath + "/frontend/lobby/lobby.html");
+});
 
 module.exports = router;
