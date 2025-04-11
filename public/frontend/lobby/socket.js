9936ec170aa2506756024117e08470de55b19aeb
Clément MASQUELIER <clement.masquelier@student.junia.com>
2025-04-11 11:20:55 +0200

    connexion plusieurs joueurs

--- public/frontend/lobby/socket.js
+++ public/frontend/lobby/socket.js
@@ -5,6 +5,8 @@ const pseudo = sessionStorage.getItem("gameName");
 const userId = sessionStorage.getItem("gameId");
 
 if (roomId){
+    socket.emit("rejoinRoom", { roomId, userId });
+
     socket.emit("getDisplayInfo", {roomId}, (data) => {
         room = data;
         if (room.error){
@@ -29,3 +31,30 @@ if (roomId){
         window.SelectedPlayerAddEventListener();
     });
 }
+socket.on("connect", ()=>{
+    
+    socket.on("updateRoomData", (data) => {
+        room = data;
+        if (room.error){
+            alert(room.error);
+            return;
+        }
+        if (room.host === userId){
+            // Host
+            window.is_host = true;
+            
+        }
+        else{
+            // Player
+            window.is_host = false;
+            document.querySelectorAll("input").forEach((elem) => {
+                elem.setAttribute("disabled", "disabled");
+              });
+        }
+        list_players.innerHTML = "";
+        room.players.forEach(player => {    
+            window.addPlayerToList(player.id, player.name, 1, player.host);
+        });
+        window.SelectedPlayerAddEventListener();
+    });
+});
